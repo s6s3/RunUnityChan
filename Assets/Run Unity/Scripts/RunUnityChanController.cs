d984394fc4854908a1c624eb06466dede137a877
Shota <Shota@SHOTA-THINK>
2016-08-18 21:20:18 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Run Unity/Scripts/RunUnityChanController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunUnityChanController : MonoBehaviour {
+    [SerializeField]
+    private UnityChanController unityChanController;
+    [SerializeField]
+    private GameObject obstaclePrefab;
+    private float elapsedTime = 0.0f;
+    private bool isGameOver = false;
+
+    // Update is called once per frame
+    void Update () {
+        if (this.isGameOver)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+            if(Physics.Raycast(ray,out raycastHit))
+            {
+                if (raycastHit.transform.gameObject.tag.Contains("UnityChan"))
+                {
+                    this.unityChanController.OnTapped();
+                }
+            }
+            
+        }
+
+        elapsedTime += Time.deltaTime;
+
+        if (1.5f <= elapsedTime)
+        {
+            GameObject obstacle = Instantiate(this.obstaclePrefab);
+            ObstacleController obstacleController = obstacle.GetComponent<ObstacleController>();
+            obstacleController.CollidedWithUnityChan += this.ObstacleCollidedWithUnityChan;
+            obstacle.transform.position = new Vector3(0.0f, 0.0f, 3.0f);
+            elapsedTime = 0.0f;
+
+        }
+        
+    }
+
+    private void ObstacleCollidedWithUnityChan()
+    {
+        if (this.isGameOver)
+        {
+            return;
+        }
+        this.unityChanController.OnCollidedWithObstacle();
+        this.isGameOver = true;
+    }
+}
---
+++ Assets/Run Unity/Scripts/UnityChanController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour {
+
+    public void OnTapped()
+    {
+        this.GetComponent<Animator>().SetTrigger("Jump");
+    }
+
+    public void OnCollidedWithObstacle()
+    {
+        this.GetComponent<Animator>().SetTrigger("Collision");
+    }
+
+    public void OnCallChangeFace()
+    {
+
+    }
+}
